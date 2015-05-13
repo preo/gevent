87d8c60da480acd9080ef2db3de043872c168b56
Robert Estelle <robert@getpreo.com>
2015-05-12 20:37:07 -0400

    Fix ThreadResult not handling exceptinons; remove apply_e.

--- gevent/resolver_thread.py
+++ gevent/resolver_thread.py
@@ -8,8 +8,6 @@ __all__ = ['Resolver']
 
 class Resolver(object):
 
-    expected_errors = Exception
-
     def __init__(self, hub=None):
         if hub is None:
             hub = get_hub()
@@ -25,16 +23,16 @@ class Resolver(object):
     # below are thread-safe in Python, even if they are not thread-safe in C.
 
     def gethostbyname(self, *args):
-        return self.pool.apply_e(self.expected_errors, _socket.gethostbyname, args)
+        return self.pool.apply(_socket.gethostbyname, args)
 
     def gethostbyname_ex(self, *args):
-        return self.pool.apply_e(self.expected_errors, _socket.gethostbyname_ex, args)
+        return self.pool.apply(_socket.gethostbyname_ex, args)
 
     def getaddrinfo(self, *args, **kwargs):
-        return self.pool.apply_e(self.expected_errors, _socket.getaddrinfo, args, kwargs)
+        return self.pool.apply(_socket.getaddrinfo, args, kwargs)
 
     def gethostbyaddr(self, *args, **kwargs):
-        return self.pool.apply_e(self.expected_errors, _socket.gethostbyaddr, args, kwargs)
+        return self.pool.apply(_socket.gethostbyaddr, args, kwargs)
 
     def getnameinfo(self, *args, **kwargs):
-        return self.pool.apply_e(self.expected_errors, _socket.getnameinfo, args, kwargs)
+        return self.pool.apply(_socket.getnameinfo, args, kwargs)
